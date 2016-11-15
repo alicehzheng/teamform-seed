04e226fb80c7fced35f2b7f67e9aaf546d55a8f6
Alice Hilary Zheng <alicehzheng@gmail.com>
2016-11-15 17:43:44 +0800

    implemented the basic function for searching the firebase

--- app/js/search.js
+++ app/js/search.js
@@ -1,24 +1,28 @@
-
 var app = angular.module("teamformApp"); 
 
-app.controller("searchCtrl", 
+app.controller("searchCtrl", ['$scope',
 	function($scope) {
 		$scope.searchText = "Search your Team!";
 		$scope.startSearch = function() {
 			var text = $scope.searchText;
 			var team = null;
-			var query = firebase.database().ref("TeamForm/teams");
+			var database = firebase.database();
+			var query = database.ref("TeamForm/teams/");
 			query.once("value").then(function(snapshot) {
-			snapshot.forEach(function(childSnapshot) {
-				var key = childSnapshot.key;
-				if(key == text || childSnapshot.child("tags/Art/" + text).exists() ||  childSnapshot.child("tags/Technology").exists()){
-					team = key;
-					return true;
-				}
-			})
+				snapshot.forEach(function(childSnapshot) {
+					var key = childSnapshot.key;
+					if(key == text || childSnapshot.child("tags/Art/" + text).exists() ||  childSnapshot.child("tags/Technology").exists()){
+						team = key;
+						return true;
+					}
+				});
 			if(team == null) {
 				$scope.searchText = "Team doesn't exist, search another one!";
 			}
-		});
-	};
-})
+			else{
+				$scope.searchText = team;
+			}
+			
+		}); 
+	}
+}])
